508005bdb0ccafb97ab27d6da66c95218bd38a0f
Sora1707 <volan1301@gmail.com>
2021-09-01 16:02:01 +0700

    first commit

---
+++ blog/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+// route
+app.get('/sora', (req, res) => {
+    return res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
